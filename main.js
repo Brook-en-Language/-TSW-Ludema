b7c61f1e570db980c51f3bc1c6aceb6e610c8494
Brook-en-Language <matteobilotti2004@gmail.com>
2025-05-22 22:02:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const bi = document.getElementById("imp");
 const gi = document.getElementById("gio");
 const cn = document.getElementById("con");
+const nc = document.getElementById("noc");
+const n1 = document.getElementById("noc1");
+const n2 = document.getElementById("noc2");
 bi.addEventListener("click", apriBI);
 gi.addEventListener("click", apriGI)
+n1.addEventListener("click", apriPartita);
 
 function apriBI(){
     cn.style.display = "flex";
 }
 
 function apriGI(){
-    cn.style.display = "flex";
+    nc.style.display = "flex";
+    n1.style.display = "flex";
+    n2.style.display = "flex";
 }
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape' && cn.style.display === 'flex') {
         cn.style.display = 'none';
+    } else if (event.key === 'Escape' && nc.style.display === 'flex') { 
+        nc.style.display = 'none'; 
     }
 });
+
+function apriPartita(){
+     window.location.href = "game.html";
+}
---
+++ secondary.js
@@ -0,0 +1,5 @@
+const pulsante1 = document.getElementById("P1");
+const pulsante2 = document.getElementById("P2");
+const pulsante3 = document.getElementById("P3");
+const pulsante4 = document.getElementById("P4");
+const pulsante5 = document.getElementById("P5");
