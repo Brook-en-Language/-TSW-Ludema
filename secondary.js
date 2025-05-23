bca68b0f0ca9e69e471061370ad13fd59d77df92
DAVIDEPALMIERI <palmieri.2058889@studenti.uniroma1.it>
2025-05-23 15:55:57 +0200

    Update secondary.js

--- secondary.js
+++ secondary.js
@@ -3,3 +3,19 @@ const pulsante2 = document.getElementById("P2");
 const pulsante3 = document.getElementById("P3");
 const pulsante4 = document.getElementById("P4");
 const pulsante5 = document.getElementById("P5");
+
+function inviaRisposta(patternId, valore, codice) {
+    const dati = new URLSearchParams();
+    dati.append("pattern_id", patternId);
+    dati.append("valore", valore);
+    dati.append("codice", codice);
+
+    fetch("salvaRisposta.php", {
+        method: "POST",
+        body: dati
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Risposta registrata:", data);
+    });
+}
